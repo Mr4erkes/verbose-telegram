6c87516e05bacd67205f832b4c2a8efdb55a8eea
Mr4erkes <mr4erkes@mail.ru>
2020-01-20 11:36:11 +0300

    update hellowakawaka.py
    add 'else' print if script imports

--- hellowakawaka.py
+++ hellowakawaka.py
@@ -3,3 +3,6 @@ def greet():
 
 if name == '__main__':
     greet()
+else:
+    print('how dare you lto load this script in your one?')
+    print('you are so brave! your parents should be proud of you')
